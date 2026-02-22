f6cdfc09d07f249bf1bc51cb39f4bab0a35af810
Natcha Phonkamhaeng <natchaphonkamhaeng@192.168.1.3>
2026-02-22 11:11:28 +0700

    create OOP style

--- main.py
+++ main.py
@@ -1,62 +1,71 @@
 # 21 Sep 2025
-# converts Mile to Kilometers
-# converts Pound to Kilogram
-# converts Inches to Centimeter
 
-def mile_to_km():
-    print("how many Miles do you want to convert to Kilometer ?")
-    mile = input()
-    km = float(mile) * 1.60934
-    return print(f"{mile} Mile is {km:.3f} Kilometer")
+class Convert:
+    def __init__(self):
+        pass
 
 
-def pound_to_kg():
-    print("how many Pound do you want to convert to Kilogram ?")
-    pound = input()
-    kg = float(pound) * 0.453592
-    return print(f"{pound} Pound is {kg:.3f} Kilogram")
+    def mile_to_km(self):
+        print("how many Miles do you want to convert to Kilometer ?")
+        mile = input()
+        km = float(mile) * 1.60934
+        print(f"{mile} Mile is {km:.3f} Kilometer")
 
 
-def foot_to_cm():
-    print("how many Foot do you want to convert to Centimeter ?")
-    foot = input()
-    cm = float(foot) * 30.48
-    return print(f"{foot} Foot is {cm:.3f} Centimeter")
+    def pound_to_kg(self):
+        print("how many Pound do you want to convert to Kilogram ?")
+        pound = input()
+        kg = float(pound) * 0.453592
+        print(f"{pound} Pound is {kg:.3f} Kilogram")
 
 
-def title():
-    us_mile = "1 : Mile to Kilometer"
-    us_pound = "2 : Pound to Kilogram"
-    us_foot = "3 : Foot to Centimeter"
-
-    print("Select your units")
-    print(us_mile)
-    print(us_pound)
-    print(us_foot)
+    def foot_to_cm(self):
+        print("how many Foot do you want to convert to Centimeter ?")
+        foot = input()
+        cm = float(foot) * 30.48
+        print(f"{foot} Foot is {cm:.3f} Centimeter")
 
 
-def main():
+    def title(self):
+        us_mile = "1 : Mile to Kilometer"
+        us_pound = "2 : Pound to Kilogram"
+        us_foot = "3 : Foot to Centimeter"
 
-    while True:
+        print("Select your units")
+        print(us_mile)
+        print(us_pound)
+        print(us_foot)
 
-        title()
-        user_select_unit = input("What items do you want to convert ?: ")
-        try:
-            user_select_unit = int(user_select_unit)
-            if user_select_unit == 1:
-                mile_to_km()
-            if user_select_unit == 2:
-                pound_to_kg()
-            if user_select_unit == 3:
-                foot_to_cm()
-        except:
-            print("please input the number")
 
-        print("Do you want to convert the units again ?")
-        ask_again = input("Y/N?: ").upper()
-        if ask_again == "N":
-            break
-    
+    def main(self):
+
+        while True:
+
+            self.title()
+            user_select_unit = input("What items do you want to convert ?: ")
+            try:
+                user_select_unit = int(user_select_unit)
+                if user_select_unit == 1:
+                    self.mile_to_km()
+                if user_select_unit == 2:
+                    self.pound_to_kg()
+                if user_select_unit == 3:
+                    self.foot_to_cm()
+
+                print("Do you want to convert the units again ?")
+                ask_again = input("Y/N?: ").upper()
+                if ask_again == "N":
+                    break
+            except:
+                print("please input the number")
+
+            # print("Do you want to convert the units again ?")
+            # ask_again = input("Y/N?: ").upper()
+            # if ask_again == "N":
+            #     break
+
+convert = Convert()
+convert.main()  
 
 if __name__ == "__main__":
-    main()
+    convert.main()
